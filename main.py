098c4a26a1ca50a0a621bba0901a5d8aa3ddc2bd
Jinho Hyeon <neverlish@gmail.com>
2023-12-03 13:01:26 +0900

    cors

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from typing import Optional
 
-from fastapi import FastAPI
-
 from address.list import fetch_addresses
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "https://frontend-e9r.pages.dev/",
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
